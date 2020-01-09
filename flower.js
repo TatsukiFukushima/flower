2e13137f0bf290e2ad220e044b651924116e5ddf
TatsukiFukushima <tatsuki2222net@gmail.com>
2020-01-09 12:14:52 +0900

    レスポンシブ的な感じにした

--- flower.js
+++ flower.js
@@ -1,20 +1,33 @@
-const r1 = 106;
-const r2 = r1 * (0.2 + Math.random() * 0.8);
+function setup() {
+    halfWidth = windowWidth * 0.5;
+    halfHeight = windowHeight * 0.5;
+    if (halfHeight > halfWidth) {
+        r = halfWidth * 0.9;
+    } else {
+        r = halfHeight * 0.9;
+    }
+    r1 = r * 0.424;
+    r2 = r1 * (0.2 + Math.random() * 0.8);
+
+    createCanvas(halfWidth * 2, halfHeight * 2);
+    background(7);
+    point = new Point(r1, r2);
+}
+
 const colorR = 150 + Math.random() * 105;
 const colorG = 150 + Math.random() * 105;
 const colorB = 150 + Math.random() * 105;
-
-function setup() {
-    createCanvas(600, 600);
-    background(7);
-    point = new Point(r1, r2);
+if (Math.random() < 0.5) {
+    var t = 0.05;
+} else {
+    var t = -0.05;
 }
 
 function draw() {
     strokeWeight(1);
     stroke(200);
     fill(0, 2);
-    ellipse(300, 300, 480, 480);
+    ellipse(halfWidth, halfHeight, r * 2, r * 2);
 
     point.move();
     point.draw();
@@ -24,20 +37,20 @@ class Point {
     constructor(r1, r2) {
         this.r1 = r1;
         this.r2 = r2;
-        this.s = 0;
-        this.x = 540 - r1 + r2;
-        this.y = 300;
+        this.t = 0;
+        this.x = halfWidth + r - r1 + r2;
+        this.y = halfHeight;
     }
 
     move() {
-        this.s += 0.05;
+        this.t += t;
     }
 
     draw() {
         var beforeX = this.x;
         var beforeY = this.y;
-        this.x = 300 + (240-this.r1) * cos(this.s) + this.r2 * cos(this.s * (this.r1 - 240) / this.r1);
-        this.y = 300 + (240-this.r1) * sin(this.s) + this.r2 * sin(this.s * (this.r1 - 240) / this.r1);
+        this.x = halfWidth + (r - this.r1) * cos(this.t) + this.r2 * cos(this.t * (this.r1 - r) / this.r1);
+        this.y = halfHeight + (r - this.r1) * sin(this.t) + this.r2 * sin(this.t * (this.r1 - r) / this.r1);
         strokeWeight(3);
         stroke(colorR, colorG, colorB);
         line(beforeX, beforeY, this.x, this.y);
